8f5518b118d22ba6720831218b59aede90215872
AbrahamMoule <abrahammougle76@gmail.com>
2024-11-03 09:12:14 +0100

    le header entete est bien fait et peu d'ajustement

--- Rent.js
+++ Rent.js
@@ -1,12 +1,16 @@
-const container = document.getElementById('container')
-const slider=document.getElementById('slider')
-const childContainer=container.children
+// Récupération des éléments DOM
+const container = document.getElementById('container');
+const slider = document.getElementById('slider');
+const dropdownMenu = document.getElementById("dropdownMenu");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
 
-slider.addEventListener('input', ()=>{
+slider.addEventListener('input', () => {
     const value = slider.value; 
-            container.style.transform = `translateX(-${value/5}%)`;
-            console.log(value);
-            
-    
-})
+    container.style.transform = `translateX(-${value / 5}%)`;
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+    hamburgerIcon.addEventListener("click", () => {
+        dropdownMenu.classList.toggle("show"); 
+    });
+});
